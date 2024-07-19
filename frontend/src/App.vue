d9ad5edb99ef99cc17897cbffebefbd9aeb66ac9
afnamal <afnamal@hotmail.com>
2024-07-19 14:37:50 +0300

    firt version

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -41,6 +41,8 @@ export default {
       successMessage: '',
       errorMessage: '',
       frames: [],
+      loading: false,
+      intervalId: null,  // interval ID'yi sakla
     };
   },
   methods: {
@@ -60,6 +62,9 @@ export default {
       formData.append('video', this.videoFile);
 
       try {
+        this.loading = true;
+        this.frames = [];  // Önceki kareleri temizle
+        this.clearCheckFramesInterval();  // Önceki interval'i temizle
         await axios.post('http://127.0.0.1:5001/upload_video', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
@@ -67,28 +72,39 @@ export default {
         });
         this.errorMessage = '';
         this.successMessage = 'Video başarıyla yüklendi ve kareler çıkarılıyor.';
-        this.fetchFrames();
+        this.checkFrames();  // Kareleri kontrol etmeye başla
       } catch (error) {
         this.successMessage = '';
         this.errorMessage = error.response ? error.response.data : error.message;
       }
     },
-    async fetchFrames() {
-      // Karelerin çıkarılmasına izin vermek için bir gecikme simüle edin
-      setTimeout(async () => {
+    clearCheckFramesInterval() {
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+      }
+    },
+    async checkFrames() {
+      // Karelerin çıkarılmasına izin vermek için belirli aralıklarla kontrol et
+      this.intervalId = setInterval(async () => {
         try {
           const response = await axios.get('http://127.0.0.1:5001/frames');
-          this.frames = response.data.map((frame) => ({
-            url: `http://127.0.0.1:5001/frames/${frame}`,
-            prediction: null,
-          }));
-          this.frames.forEach(frame => {
-            this.predictFire(frame);
-          });
+          if (response.status === 200) {
+            this.frames = response.data.map((frame) => ({
+              url: `http://127.0.0.1:5001/frames/${frame}`,
+              prediction: null,
+            }));
+            this.clearCheckFramesInterval();  // Tüm kareler çıkarıldıktan sonra kontrolü durdur
+            this.frames.forEach(frame => {
+              this.predictFire(frame);
+            });
+          } else if (response.status === 202) {
+            console.log('Kare çıkarma işlemi devam ediyor...');
+          }
         } catch (error) {
           this.errorMessage = error.response ? error.response.data : error.message;
         }
-      }, 5000); // 5 saniye gecikme
+      }, 3000);  // 3 saniye aralıklarla kontrol et
     },
     async predictFire(frame) {
       try {
@@ -106,6 +122,9 @@ export default {
       }
     },
   },
+  beforeUnmount() {
+    this.clearCheckFramesInterval();  // Bileşen yok edildiğinde interval'i temizle
+  }
 };
 </script>
 
--- prediction/fire_detection.py
+++ prediction/fire_detection.py
@@ -7,22 +7,43 @@ from PIL import Image
 import io
 import os
 import requests
+from dotenv import load_dotenv
+
+# Ortam değişkenlerini yükle
+load_dotenv()
 
 # Ortam değişkenini ayarla
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
+# Model yolunu yazdır
+model_path = "./final_model.h5"
+print(f"Model path: {model_path}")
+
 # Modeli yükle
-model_path = "./final_model.h5"  # İndirilen modelin yolu
 model = None
 try:
     model = load_model(model_path)
-    model.summary()  # Modelin yapısını çıktıya ver
+    model.summary()
 except Exception as e:
     print(f"Model yüklenirken hata oluştu: {e}")
 
+# Sayaç dosyasının yolunu belirle
+counter_path = "/tmp/counter.txt"
+
+def get_counter():
+    """Sayaç dosyasını oku veya oluştur ve sayaç değerini döndür"""
+    if not os.path.exists(counter_path):
+        with open(counter_path, 'w') as f:
+            f.write('0')
+    with open(counter_path, 'r') as f:
+        count = int(f.read().strip())
+    with open(counter_path, 'w') as f:
+        f.write(str(count + 1))
+    return count
+
 def preprocess_image(image):
     """Resmi ön işleme"""
-    image = image.resize((300, 300))  # Modelinizin beklediği giriş boyutuna göre ayarlayın
+    image = image.resize((300, 300))
     image = np.array(image)
     if image.shape[-1] == 4:  # RGBA görüntüler için alfa kanalını çıkarın
         image = image[..., :3]
@@ -36,18 +57,33 @@ def predict_fire(image):
     fire_prob = predictions[0][0]
     return ("fire", fire_prob) if fire_prob >= 0.5 else ("no fire", 1 - fire_prob)
 
-def send_telegram_message(message):
+def send_telegram_message(message, image_path):
     """Telegram botuna mesaj gönderme"""
-    bot_token = '7320273980:AAE0PUg-wC0DkYUiuQpBjxaqs4DV36hR2co'
-    chat_id = '1010415776'
+    bot_token = os.getenv('BOT_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    print(f"Bot token: {bot_token}")
+    print(f"Chat ID: {chat_id}")
     telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    telegram_photo_url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
     
     try:
         response = requests.post(telegram_url, data={
             'chat_id': chat_id,
             'text': message,
         })
-        return response.status_code == 200
+        if response.status_code != 200:
+            print(f"Telegram mesaj gönderilemedi: {response.text}")
+            return False
+
+        with open(image_path, 'rb') as image_file:
+            response = requests.post(telegram_photo_url, data={
+                'chat_id': chat_id,
+            }, files={'photo': image_file})
+            if response.status_code != 200:
+                print(f"Telegram fotoğraf gönderilemedi: {response.text}")
+                return False
+        
+        return True
     except Exception as e:
         print(f"Telegram mesaj gönderilemedi: {e}")
         return False
@@ -70,7 +106,10 @@ def predict():
 
         if prediction_class == "fire":
             message = f"Yangın Tespit Edildi! Olasılık: {prediction_prob:.2f}"
-            send_telegram_message(message)
+            count = get_counter()
+            image_path = f"/tmp/detected_fire_{count}.jpg"
+            image.save(image_path)
+            send_telegram_message(message, image_path)
 
         return jsonify({'predicted_class': prediction_class, 'probability': float(prediction_prob)})
     except Exception as e:
